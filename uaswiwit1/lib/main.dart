000f51fb8437e2bbdae1d561e7ade09a40b78d6a
WiwitLastianto <57537026+WiwitLastianto@users.noreply.github.com>
2020-01-18 01:47:11 +0800

    Tugas Visual

---
+++ uaswiwit1/lib/main.dart
@@ -0,0 +1,379 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'package:flutter/material.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:async';
+import 'Detail.dart';
+import 'AddData.dart';
+
+void main() {
+ runApp(MaterialApp(
+   home: Login(),
+   theme: ThemeData(),
+   debugShowCheckedModeBanner: false,
+ ));
+}
+
+class Login extends StatefulWidget {
+ @override
+ _LoginState createState() => _LoginState();
+}
+
+enum LoginStatus { notSignIn, signIn }
+
+class _LoginState extends State<Login> {
+ LoginStatus _loginStatus = LoginStatus.notSignIn;
+ String email, password;
+ final _key = new GlobalKey<FormState>();
+
+ bool _secureText = true;
+
+ showHide() {
+   setState(() {
+     _secureText = !_secureText;
+   });
+ }
+
+ check() {
+   final form = _key.currentState;
+   if (form.validate()) {
+     form.save();
+     login();
+   }
+ }
+
+ login() async {
+   final response = await http.post("http://visual3.000webhostapp.com/pegawai/login.php",body: {
+     "email": email, 
+     "password": password
+     });
+     
+   final data = jsonDecode(response.body);
+   int value = data['value'];
+   String pesan = data['message'];
+   String emailAPI = data['email'];
+   String namaAPI = data['nama'];
+   String id = data['id'];
+   if (value == 1) {
+     setState(() {
+       _loginStatus = LoginStatus.signIn;
+       savePref(value, emailAPI, namaAPI, id);
+     });
+     print(pesan);
+   } else {
+     print(pesan);
+   }
+ }
+
+ savePref(int value, String email, String nama, String id) async {
+   SharedPreferences preferences = await SharedPreferences.getInstance();
+   setState(() {
+     preferences.setInt("value", value);
+     preferences.setString("nama", nama);
+     preferences.setString("email", email);
+     preferences.setString("id", id);
+     preferences.commit();
+   });
+ }
+
+ var value;
+ getPref() async {
+   SharedPreferences preferences = await SharedPreferences.getInstance();
+   setState(() {
+     value = preferences.getInt("value");
+
+     _loginStatus = value == 1 ? LoginStatus.signIn : LoginStatus.notSignIn;
+   });
+ }
+
+ signOut() async {
+   SharedPreferences preferences = await SharedPreferences.getInstance();
+   setState(() {
+     preferences.setInt("value", null);
+     preferences.commit();
+     _loginStatus = LoginStatus.notSignIn;
+   });
+ }
+
+ @override
+ void initState() {
+   // TODO: implement initState
+   super.initState();
+   getPref();
+ }
+
+ @override
+ Widget build(BuildContext context) {
+   switch (_loginStatus) {
+     case LoginStatus.notSignIn:
+       return Scaffold(
+         appBar: AppBar(
+           title: Text("SELAMAT DATANG DI APLIKASIKU"),
+         ),
+         body: Form(
+           key: _key,
+           child: ListView(
+             padding: EdgeInsets.all(16.0),
+             children: <Widget>[
+               Image.network('https://i0.wp.com/wallur.com/wp-content/uploads/2016/12/panda-background-6.jpg?fit=1920%2C1080',
+                width: 300.0,
+                height: 150.0,),
+               TextFormField(
+                 validator: (e) {
+                   if (e.isEmpty) {
+                     return "Please insert email";
+                   }
+                 },
+                 onSaved: (e) => email = e,
+                 decoration: InputDecoration(
+                   labelText: "Email",
+                 ),
+               ),
+               TextFormField(
+                 obscureText: _secureText,
+                 onSaved: (e) => password = e,
+                 decoration: InputDecoration(
+                   labelText: "Password",
+                   suffixIcon: IconButton(
+                     onPressed: showHide,
+                     icon: Icon(_secureText
+                         ? Icons.visibility_off
+                         : Icons.visibility),
+                   ),
+                 ),
+               ),
+               MaterialButton(
+                 onPressed: () {
+                   check();
+                 },
+                 child: Text("Login"),
+               ),
+               InkWell(
+                 onTap: () {
+                   Navigator.of(context).push(
+                       MaterialPageRoute(builder: (context) => Register()));
+                 },
+                 child: Text(
+                   "Buat akun baru",
+                   textAlign: TextAlign.center,
+                 ),
+               )
+             ],
+           ),
+         ),
+       );
+       break;
+     case LoginStatus.signIn:
+       return MainMenu(signOut);
+       break;
+   }
+ }
+}
+class Register extends StatefulWidget {
+ @override
+ _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+ String email, password, nama;
+ final _key = new GlobalKey<FormState>();
+
+ bool _secureText = true;
+
+ showHide() {
+   setState(() {
+     _secureText = !_secureText;
+   });
+ }
+
+ check() {
+   final form = _key.currentState;
+   if (form.validate()) {
+     form.save();
+     save();
+   }
+ }
+
+ save() async {
+   final response = await http.post("http://visual3.000webhostapp.com/pegawai/register.php",
+       body: {"nama": nama, "email": email, "password": password});
+   final data = jsonDecode(response.body);
+   int value = data['value'];
+   String pesan = data['message'];
+   if (value == 1) {
+     setState(() {
+       Navigator.pop(context);
+     });
+   } else {
+     print(pesan);
+   }
+ }
+
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       title: Text("Register"),
+     ),
+     body: Form(
+       key: _key,
+       child: ListView(
+         padding: EdgeInsets.all(16.0),
+         children: <Widget>[
+           TextFormField(
+             validator: (e) {
+               if (e.isEmpty) {
+                 return "Please insert fullname";
+               }
+             },
+             onSaved: (e) => nama = e,
+             decoration: InputDecoration(labelText: "Nama Lengkap"),
+           ),
+           TextFormField(
+             validator: (e) {
+               if (e.isEmpty) {
+                 return "Please insert email";
+               }
+             },
+             onSaved: (e) => email = e,
+             decoration: InputDecoration(labelText: "email"),
+           ),
+           TextFormField(
+             obscureText: _secureText,
+             onSaved: (e) => password = e,
+             decoration: InputDecoration(
+               labelText: "Password",
+               suffixIcon: IconButton(
+                 onPressed: showHide,
+                 icon: Icon(
+                     _secureText ? Icons.visibility_off : Icons.visibility),
+               ),
+             ),
+           ),
+           MaterialButton(
+             onPressed: () {
+               check();
+             },
+             child: Text("Register"),
+           )
+         ],
+       ),
+     ),
+   );
+ }
+}
+
+class MainMenu extends StatefulWidget {
+ final VoidCallback signOut;
+ MainMenu(this.signOut);
+ @override
+ _MainMenuState createState() => _MainMenuState();
+ 
+}
+
+class _MainMenuState extends State<MainMenu> {
+  //  method yg kita gunakan untuk mengambil data dari server
+  Future<List> getData() async {
+    final response = await http.get("http://visual3.000webhostapp.com/pegawai/getPegawai.php");
+    return json.decode(response.body); // hanya body yang di ambil
+  }
+ signOut() {
+   setState(() {
+     widget.signOut();
+   });
+ }
+
+//  String email = "", nama = "";
+//  TabController tabController;
+
+ getPref() async {
+   SharedPreferences preferences = await SharedPreferences.getInstance();
+   setState(() {
+//      email = preferences.getString("email");
+//      nama = preferences.getString("nama");
+   });
+ }
+
+ @override
+ void initState() {
+   // TODO: implement initState
+   super.initState();
+   getPref();
+ }
+
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+    debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: new AppBar(
+          title: Text("Nama Para Pegawai"),
+          actions: <Widget>[
+           IconButton(
+             onPressed: () {
+               signOut();
+             },
+             icon: Text("Log out")
+           )
+         ],
+          backgroundColor: Colors.blue[400],
+        ),
+        floatingActionButton: new FloatingActionButton(
+          child: new Icon(
+            Icons.person_add,
+            color: Colors.black,
+          ),
+          onPressed: () => Navigator.of(context).push(new MaterialPageRoute(
+            builder: (BuildContext context) => new AddData(),
+          )),
+        ),
+        body: new FutureBuilder<List>(
+          future: getData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) print(snapshot.error);
+            return snapshot.hasData
+                ? new ItemList(
+                    list: snapshot.data,
+                  )
+                : new Center(
+                    child: new CircularProgressIndicator(),
+                  );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class ItemList extends StatelessWidget {
+  final List list;
+  ItemList({this.list});
+  Widget build(BuildContext context) {
+    return new ListView.builder(
+        itemCount: list == null ? 0 : list.length,
+        itemBuilder: (context, i) {
+          return new Container(
+            child: new GestureDetector(
+              onTap: () => Navigator.of(context).push(new MaterialPageRoute(
+                  builder: (BuildContext context) => new Detail(
+                        list: list,
+                        index: i,
+                      ))),
+              child: new Card(
+                color: Colors.cyan[100],
+                child: new ListTile(
+                  title: new Text(list[i]['nama']),
+                  leading: new Icon(Icons.person),
+                  subtitle: new Text("Posisi : ${list[i]['posisi']}"),
+                ),
+              ),
+            ),
+          );
+        
+        });
+  }
+}
+ 
